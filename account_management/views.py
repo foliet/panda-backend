67a01bb96fb1a0308850aa7224fe482665a175bc
duerwuyi <1124457432@qq.com>
2022-02-17 19:22:47 +0800

    改写了登陆.

--- account_management/views.py
+++ account_management/views.py
@@ -12,6 +12,7 @@ from account_management.forms import UserForm
 
 # Create your views here.
 from course.serializers import UserSerializer
+from pandaBackend.Result import Result
 
 
 class LoginForm(forms.Form):
@@ -19,7 +20,6 @@ class LoginForm(forms.Form):
     password = forms.CharField(label='密码', widget=forms.PasswordInput())
 
     def clean_email(self):
-        print('clean')
         value = self.cleaned_data['email']
         try:
             result_email = User.objects.get(email=value)
@@ -29,10 +29,11 @@ class LoginForm(forms.Form):
 
     def clean_password(self):
         value = self.cleaned_data['password']
-        if re.match('^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,20}$', value):
+        #这个正则表达式不一定对
+        if re.match('^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z!@#$%&*]{6,20}$', value):
             return value
         else:
-            raise forms.ValidationError(u"密码必须由6-20个字母和数字组成", code='password invalid')
+            raise forms.ValidationError(u"密码必须由6-20个字母和数字或!@#$%&*组成", code='password invalid')
 
 
 def register(request):
@@ -58,7 +59,6 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        print(request.POST)
         loginform = LoginForm(request.POST)
         if loginform.is_valid():
             email = loginform.cleaned_data['email']
@@ -69,11 +69,13 @@ def login(request):
             if user:
                 request.session['email'] = email
                 request.session['is_login'] = True
-                return HttpResponse('登陆成功')
+                return JsonResponse(data=Result(user[0].username, message="登陆成功").toDict())
             else:
-                return HttpResponse('用户名或密码错误,请重新登录')
+                return JsonResponse(data=Result("", message="用户名或密码错误,请重新登录", status=False, code=101).toDict())
         else:
-            return HttpResponse(loginform.errors.as_json())
+            return JsonResponse(data=Result("", message=loginform.errors.get("email", "")+
+                                                        loginform.errors.get("password", ""),
+                                            status=False, code=102).toDict())
 
 
 def index(request):
@@ -81,7 +83,7 @@ def index(request):
         # 提取浏览器中的cookie，如果不为空，表示已经登录
         u = request.session.get('email', None)
         is_login = request.session.get('is_login', None)
-        return JsonResponse('用户{0}的登录状态是{1}'.format(u, is_login), safe=False)
+        return JsonResponse(Result('用户{0}的登录状态是{1}'.format(u, is_login)).toDict())
 
 
 def logout(request):
