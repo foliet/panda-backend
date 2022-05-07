ecc7e5a3dc629cc6a21042bf980f6544f75337e1
foliet <foliet@rainspace.cn>
2022-05-08 01:59:23 +0800

    fix: 替换了默认的用户名认证方式导致无法进入admin界面，通过允许双重认证方式解决

--- course/admin.py
+++ course/admin.py
@@ -1,1 +1,11 @@
 # Register your models here.
+from django.contrib import admin
+
+from .models import VideoModel, Grammar, Category, Sentence, Word, Advertisement
+
+admin.site.register(VideoModel)
+admin.site.register(Grammar)
+admin.site.register(Category)
+admin.site.register(Sentence)
+admin.site.register(Word)
+admin.site.register(Advertisement)
--- panda/settings.py
+++ panda/settings.py
@@ -126,7 +126,7 @@ CACHES = {
 # User
 AUTH_USER_MODEL = 'authentication.User'
 
-AUTHENTICATION_BACKENDS = ['panda.backends.EmailModelBackend']
+AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend', 'panda.backends.EmailModelBackend']
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
